fe62a18860ba7e7a4240231a8d7097d5d5ddc19d
The0z <63734739+The0z@users.noreply.github.com>
2020-11-24 21:57:24 -0700

    Welcome PopUp Created.

--- base-project/script.js
+++ base-project/script.js
@@ -1,1 +1,16 @@
 // The JavaScript file!
+
+document.getElementById("teamName").addEventListener("input", checkTeamField);
+
+function checkTeamField(){
+    var teamName = document.getElementById('teamName');
+    var btn = document.getElementById('teamBtn');
+    console.log(teamName.value);
+    if(teamName.value === ''){
+        btn.disabled = true;
+    }
+    else{
+        btn.disabled = false;
+    }
+}
+
