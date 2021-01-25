bb1842b694098d5911548eae494c2f5726f8b834
The0z <63734739+The0z@users.noreply.github.com>
2021-01-25 12:55:41 -0700

    Changed from session storage to cookies.

--- base-project/indexScript.js
+++ base-project/indexScript.js
@@ -5,7 +5,6 @@ document.getElementById("teamName").addEventListener("input", checkTeamField);
 
 // Check if team name is empty, if not then continue on.
 function checkTeamField(){
-    //var teamName = document.getElementById('teamName');
     var btn = document.getElementById('teamBtn');
     if(teamName.value.length > 50){
         alert("Please Enter a Team Name less or equal to 50 characters");
@@ -20,15 +19,29 @@ function checkTeamField(){
     }
 }
 
-document.getElementById('teamBtn').addEventListener("click", function(){
-   console.log("Button Clicked!");
-   //if web storage exists then do this, otherwise send window alert
-   if(typeof(Storage) !== undefined){
-       sessionStorage.setItem('teamName', teamName.value);
-       //May need to change if website folder setup changes.
-       window.location.href = "questions/questions.html";
-   } else {
-       alert("Sorry, your browser doesn't support session storage. Please use a different browser to play the game");
-   }
+//Checks to see if only letters and spaces were used for name
+function checkLettersSpaces(textInput){
+    //RegEx enclosed with / ... / 
+    // ^ = begin with, [a-zA-Z-,] = any lower/uppercase letters, comma, or dash
+    // + = 1 more times of the preceding, \s white space, {0,1} match 0 or 1 times whitespace changed to *
+    // () = group, * = 0 or more times of thing proceding it, $ end!
+    var filter = /^[a-zA-Z-,]+(\s*[a-zA-Z-,])*$/;
+    if(textInput.value.match(filter)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+document.getElementById('teamBtn').addEventListener("click", function(){    
+    if(checkLettersSpaces(teamName)){
+        document.cookie = 'teamname=' + teamName.value;
+        window.location.href = "questions/questions.html";
+    }
+    else{
+        alert('Please enter only letters, dash, commas, and/or spaces');
+    }
 });
 
