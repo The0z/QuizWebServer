ca4d049737d98516e1bfb02fc7f28d5a5a3dc2fb
The0z <63734739+The0z@users.noreply.github.com>
2021-01-25 13:10:03 -0700

    Updated to use cookies

--- base-project/questions/questionsScript.js
+++ base-project/questions/questionsScript.js
@@ -2,8 +2,9 @@
 
 //On load set teamName that was retreived earlier. If somehow null set it to BugFinder Team
 window.onload = (event) => {
-    var teamName = sessionStorage.getItem('teamName');
-    if(teamName == null){
+    var teamName = getCookie('teamname');
+    //var teamName = sessionStorage.getItem('teamName');
+    if(teamName == null || teamName == ''){
         teamName = 'Bug Finder Team';
     }
     document.getElementById('teamName').innerHTML = teamName;
@@ -15,3 +16,20 @@ document.getElementById('formBtn').addEventListener("click", function(){
         document.getElementById('testForm').submit();
     }
 });
+
+// https://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie); //decode cookie string to handle cookies with special characters.
+    var ca = decodedCookie.split(';'); //split decoded cookie based on semi-colons.
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) { //if cookie is found do this
+        return c.substring(name.length, c.length);
+      }
+    }
+    return ""; //else return empty string if cookie is not found.
+  }
