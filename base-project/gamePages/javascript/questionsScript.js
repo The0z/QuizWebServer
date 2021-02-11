6e44db9824c766b85c1af39f120dd8d0345e474e
The0z <63734739+The0z@users.noreply.github.com>
2021-02-11 12:18:36 -0700

    Added easy access to photos and text selection

--- base-project/gamePages/javascript/questionsScript.js
+++ base-project/gamePages/javascript/questionsScript.js
@@ -1,5 +1,54 @@
 $(document).ready(function(){
 
+  /* Easy way to populate questions without too much automation */
+  function populateQuestions(){
+    /* Question Text */
+    $("#q1P").html("Question 1: Who the heck is this?");
+    $("#q2P").html("Question 2: Who the heck is this?");
+    $("#q3P").html("Question 3: What is the brand and name of this car?");
+    $("#q4P").html("Question 4: What is the name of this structure?");
+    $("#q5P").html("Question 5: What is this a part of?");
+    $("#q6P").html("Question 6: Which villian is this actor portraying?");
+    $("#q7P").html("Question 7: What is the name of the place below the tower?");
+    $("#q8P").html("Question 8: Who is this?");
+    $("#q9P").html("Question 9: What is the brand and name of this sport hatch?");
+    $("#q10P").html("Question 10: Who is the heck is this?");
+    $("#q11P").html("Question 11: What is thing waves in the sky called?");
+    $("#q12P").html("Question 12: Where is this?");
+    $("#q13P").html("Question 13: What is this?!?");
+    $("#q14P").html("Question 14: Where is this?");
+    $("#q15P").html("Question 15: Who drives this in this group?");
+    $("#q16P").html("Question 16: What are these super cool awesome definitely not testing formatting animals called?");
+    $("#q17P").html("Question 17: What type of dog is this?");
+    $("#q18P").html("Question 18: Who the heck is this?");
+    $("#q19P").html("Question 19: Who the heck is this?");
+    $("#q20P").html("Question 20: Who the heck is this?");
+
+    /* Question Image */
+    $("#q1Img").attr("src","game_images/q1.jpg");
+    $("#q2Img").attr("src","game_images/q2.jpg");
+    $("#q3Img").attr("src","game_images/q3.jpg");
+    $("#q4Img").attr("src","game_images/q4.jpg");
+    $("#q5Img").attr("src","game_images/q5.jpg");
+    $("#q6Img").attr("src","game_images/q6.jpg");
+    $("#q7Img").attr("src","game_images/q7.jpg");
+    $("#q8Img").attr("src","game_images/q8.jpg");
+    $("#q9Img").attr("src","game_images/q9.jpg");
+    $("#q10Img").attr("src","game_images/q10.jpg");
+    $("#q11Img").attr("src","game_images/q11.jpg");
+    $("#q12Img").attr("src","game_images/q12.jpg");
+    $("#q13Img").attr("src","game_images/q13.jpg");
+    $("#q14Img").attr("src","game_images/q14.jpg");
+    $("#q15Img").attr("src","game_images/q15.jpg");
+    $("#q16Img").attr("src","game_images/q16.jpg");
+    $("#q17Img").attr("src","game_images/q17.jpg");
+    $("#q18Img").attr("src","game_images/q18.jpg");
+    $("#q19Img").attr("src","game_images/q19.jpg");
+    $("#q20Img").attr("src","game_images/q20.jpg");
+  }
+
+
+
 //On load set teamName that was created on the previous page. If somehow null set it to BugFinder Team
 //Set team Name header to Team: teamName
 window.onload = (event) => {
@@ -9,7 +58,11 @@ window.onload = (event) => {
         teamName = 'Bug Finder Team';
     }
     document.getElementById('teamName').innerHTML = "Team: " + teamName;
+    populateQuestions();
 }
+
+
+
 
   //Form Button used to move onto next webpage
   $("#formBtn").click(function (e) {
